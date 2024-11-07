1bdb83c15941a7ce4a7d2e7652b47deb4281f960
Diego Martin <diego.martin@gofore.com>
2024-11-07 13:13:28 +0100

    Add filters to threats and code style refactor

--- src/main/java/global/govstack/threat_service/pub_sub/IMListener.java
+++ src/main/java/global/govstack/threat_service/pub_sub/IMListener.java
@@ -12,11 +12,11 @@ import org.springframework.stereotype.Component;
 public class IMListener {
     private static final String THREAT_TOPIC = "threats-topic";
 
-  private final ThreatService threatService;
+    private final ThreatService threatService;
 
-//    @KafkaListener(groupId = "threat-service-listener", topics = THREAT_TOPIC)
-//    public void handleIncomingThreatFromIM(String threatMessage){
-//        log.info("Incoming message from threats-topic");
-//        this.threatService.handleIncomingThreatFromIM(threatMessage);
-//    }
+    @KafkaListener(groupId = "threat-service-listener", topics = THREAT_TOPIC)
+    public void handleIncomingThreatFromIM(String threatMessage){
+        log.info("Incoming message from threats-topic");
+        this.threatService.handleIncomingThreatFromIM(threatMessage);
+    }
 }
