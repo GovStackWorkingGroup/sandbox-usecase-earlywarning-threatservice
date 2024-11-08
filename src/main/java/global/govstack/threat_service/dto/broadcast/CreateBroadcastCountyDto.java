b670b2899106151dca53dc9bb057d58b54e4ac49
vuk.damjanovic <vuk.damjanovic@3stepit.com>
2024-11-08 15:34:47 +0200

    broadcast mapping fixed; added test data for broadcast counties;

--- src/main/java/global/govstack/threat_service/dto/broadcast/CreateBroadcastCountyDto.java
+++ src/main/java/global/govstack/threat_service/dto/broadcast/CreateBroadcastCountyDto.java
@@ -1,6 +1,8 @@
 package global.govstack.threat_service.dto.broadcast;
 
+import lombok.Builder;
 
+@Builder
 public record CreateBroadcastCountyDto(
         Long countyId,
         String countyName) {
--- src/main/java/global/govstack/threat_service/mapper/BroadcastMapper.java
+++ src/main/java/global/govstack/threat_service/mapper/BroadcastMapper.java
@@ -2,16 +2,36 @@ package global.govstack.threat_service.mapper;
 
 import global.govstack.threat_service.dto.broadcast.BroadcastCreateDto;
 import global.govstack.threat_service.dto.broadcast.BroadcastDto;
+import global.govstack.threat_service.dto.broadcast.CreateBroadcastCountyDto;
 import global.govstack.threat_service.repository.entity.Broadcast;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface BroadcastMapper {
-  BroadcastDto entityToDto(Broadcast broadcast);
+    @Mapping(source = "broadcastUUID", target = "broadcastId")
+    @Mapping(source = "broadcast.threatEvent.threatUUID", target = "threatId")
+    @Mapping(source = "broadcast", target = "affectedCounties", qualifiedByName = "assembleAffectedCounties")
+    BroadcastDto entityToDto(Broadcast broadcast);
 
-  Broadcast dtoToEntity(BroadcastDto broadcastDto);
+    @Mapping(source = "broadcastId", target = "broadcastUUID")
+    Broadcast dtoToEntity(BroadcastDto broadcastDto);
 
-  @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
-  Broadcast createDtoToEntity(BroadcastCreateDto broadcastCreateDto);
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+    Broadcast createDtoToEntity(BroadcastCreateDto broadcastCreateDto);
+
+    @Named("assembleAffectedCounties")
+    default List<CreateBroadcastCountyDto> assembleAffectedCounties(Broadcast broadcast) {
+        return broadcast.getAffectedCounties().stream().map(b ->
+                        CreateBroadcastCountyDto
+                                .builder()
+                                .countyId(b.getCountyId())
+                                .countyName(b.getCountyName())
+                                .build())
+                .collect(Collectors.toList());
+    }
 }
